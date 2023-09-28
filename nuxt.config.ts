bbab8bd4b7525f8f3271c6d618178adb61e2f2c2
Ernesto Murrugarra <ernestomurrudev@gmail.com>
2023-09-28 13:51:22 -0300

    Second update to fix zooming in mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [{ rel: 'stylesheet', href: 'https://www.w3schools.com/w3css/4/w3.css' }],
-      meta: [{ name:"viewport", content:"width=device-width, user-scalable=no" }]
+      meta: [
+        { name:"viewport", content:"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" },
+        { name: "HandheldFriendly", content: "true" }
+      ],
     }
   },
   ssr: false
