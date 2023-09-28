6ffbd792eb1a6601f87dab8af141acb763cf283e
Ernesto Murrugarra <ernestomurrudev@gmail.com>
2023-09-28 13:45:33 -0300

    Fisrt update to fix zooming in mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   app: {
     head: {
-      link: [{ rel: 'stylesheet', href: 'https://www.w3schools.com/w3css/4/w3.css' }]
+      link: [{ rel: 'stylesheet', href: 'https://www.w3schools.com/w3css/4/w3.css' }],
+      meta: [{ name:"viewport", content:"width=device-width, user-scalable=no" }]
     }
   },
   ssr: false
